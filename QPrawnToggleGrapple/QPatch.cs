02b2bdf76def71db0749dbe8ed3bebdbdb1d0e16
TaOkay <djaxemaster@gmail.com>
2018-03-20 19:33:16 -0700

    Add .gitignore and project skeleton (excluding project files)

---
+++ QPrawnToggleGrapple/QPatch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using Harmony;
+
+namespace QPrawnToggleGrapple
+{
+    internal class QPatch
+    {
+        public static void Patch()
+        {
+            HarmonyInstance harmonyInstance = HarmonyInstance.Create("qprawn_toggle_grapple.mod");
+            harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+            QPatch.Path = (QPatch.Path ?? "QMods\\QPrawnToggleGrapple");
+            QPatch.LoadConfig();
+            Console.WriteLine("[QPrawnToggleGrapple] Patched");
+        }
+
+        private static string GetModInfoPath()
+        {
+            return Environment.CurrentDirectory + "\\" + QPatch.Path + "\\mod.json";
+        }
+
+        private static void LoadConfig()
+        {
+        }
+
+        private static string Path = null;
+    }
+}
